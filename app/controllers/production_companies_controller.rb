6a10c4697ee029f1fd94041383f72904ce9bfbc5
Patryk Jagielski <pjagielski@academic.rrc.ca>
2022-09-26 19:27:19 -0500

    first commit

--- app/controllers/production_companies_controller.rb
+++ app/controllers/production_companies_controller.rb
@@ -1,7 +1,10 @@
 class ProductionCompaniesController < ApplicationController
   def index
+    # @production_companies = ProductionCompany.all
+    @production_companies = ProductionCompany.ordered_by_movies
   end
 
   def show
+    @production_company = ProductionCompany.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  get 'home/index'
   get 'production_companies/index'
   get 'production_companies/show'
   get 'movies/index'
